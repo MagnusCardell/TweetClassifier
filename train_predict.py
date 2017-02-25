711aa5712b100ff927126f51a8cb5b95a58a3224
MagnusCardell <cardell.magnus@gmail.com>
2017-02-24 18:33:20 -0600

    count word frequency of every word of a set of tweets

--- train_predict.py
+++ train_predict.py
@@ -1,3 +1,4 @@
+import random
 import csv
 
 # opens .csv file and reads single tweets into arrays
@@ -8,30 +9,54 @@ def loadCsv(filename):
 		dataset[i] = [x for x in dataset[i]]
 	return dataset
 
-# 
+# split tweets into training and test sets
 def splitDataset(dataset, splitRatio):
 	trainSize = int(len(dataset) * splitRatio)
 	trainSet = []
 	copy = list(dataset)
-	while len(trainSet) < trainSize:
-		index = random.randrange(len(copy))
-		trainSet.append(copy.pop(index))
+	i = 0
+	while i < trainSize:
+		trainSet.append(copy[i])
+		copy.pop(i)
+		i=i+1
 	return [trainSet, copy]
+
+def separateByClass(tweets):
+	separated = {}
+	for dataset in tweets:
+		vector = ();
+		for i in range(len(dataset)):
+			vector = vector + tuple(dataset[i].split())
+		#print(len(vector))
+		#print(vector)
+
+		for i in range(len(vector)):
+			#vector = dataset[i].split()
+			if (vector[i] not in separated):
+				separated[vector[i]] = 0
+			separated[vector[i]]= separated[vector[i]]+ 1
+	return separated
 
 # main function
 if __name__ == '__main__':
 
+	test = ['1','2','3','4','3','2','3','1','2']
 	news = loadCsv("news.csv")
 	politics = loadCsv("politics.csv")
 	celebs = loadCsv("celebs.csv")
 	sports = loadCsv("sports.csv")
 
-
-
-
-
+	splitRatio = 0.5
+	train, copy = splitDataset(news, splitRatio)
+	#print(train)
 
 	i = 0
-	while i < (len(news)):
-		print(news[i])
-		i=i+1
+	separate = separateByClass(train)
+	print(separate)
+	
+
+
+	# i = 0
+	# while i < (len(news)):
+	# 	print(news[i])
+	# 	i=i+1
